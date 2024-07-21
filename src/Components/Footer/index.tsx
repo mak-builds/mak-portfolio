3dbf300aa9c79d61d561ff0126ff4b82e641baac
mAk <mak.work9601@gmail.com>
2024-07-21 23:45:09 +0530

    added bharat

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Flex, Text } from "@chakra-ui/react";
 const FooterComponent = () => {
   return (
     <Flex justifyContent={"center"} py={4}>
-      <Text>Made with ❤️ in Bharat</Text>
+      <Text>Made with ❤️ in भारत</Text>
     </Flex>
   );
 };
